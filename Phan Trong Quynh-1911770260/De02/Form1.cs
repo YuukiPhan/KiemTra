1645f786e7466b3b4350a46bf4d624476e1e6618
YuuQuinn <126931733+YuukiPhan@users.noreply.github.com>
2024-08-07 12:58:56 +0700

    Add files via upload

---
+++ Phan Trong Quynh-1911770260/De02/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using System;
+namespace QLSP
+{
+    public partial class FormSanPham : Form
+    {
+        List<SanPham> danhSachSanPham = new List<SanPham>();
+        SanPham sanPhamMoi;
+
+        public FormSanPham()
+        {
+            InitializeComponent();
+
+            // Khởi tạo dữ liệu mẫu
+            danhSachSanPham.Add(new SanPham() { MaSP = "SP001", TenSP = "Sản phẩm A", NgayNhap = DateTime.Now, LoaiSP = "Loại 1" });
+            HienThiDanhSach();
+        }
+
+        public FormSanPham(SanPham sanPham)
+        {
+            InitializeComponent();
+            sanPhamMoi = sanPham;
+        }
+
+        private void HienThiDanhSach()
+        {
+            listView1.Items.Clear();
+            foreach (SanPham sp in danhSachSanPham)
+            {
+                ListViewItem item = new ListViewItem(sp.MaSP);
+                item.SubItems.Add(sp.TenSP);
+                item.SubItems.Add(sp.NgayNhap.ToShortDateString());
+                item.SubItems.Add(sp.LoaiSP);
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            SanPham sanPhamMoi = new SanPham();
+            sanPhamMoi.MaSP = txtMaSP.Text;
+            sanPhamMoi.TenSP = txtTenSP.Text;
+            sanPhamMoi.NgayNhap = DateTime.Parse(txtNgayNhap.Text);
+            sanPhamMoi.LoaiSP = txtLoaiSP.Text;
+
+            FormSanPham frmSanPham = new FormSanPham(sanPhamMoi);
+            if (frmSanPham.ShowDialog() == DialogResult.OK)
+            {
+                danhSachSanPham.Add(frmSanPham.sanPhamMoi);
+                HienThiDanhSach();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SanPham sanPhamMoi = new SanPham();
+            FormSanPham frmSanPham = new FormSanPham(sanPhamMoi);
+            if (frmSanPham.ShowDialog() == DialogResult.OK)
+            {
+                danhSachSanPham.Add(frmSanPham.sanPhamMoi);
+                HienThiDanhSach();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem item = listView1.SelectedItems[0];
+                string maSP = item.SubItems[0].Text;
+                SanPham sp = danhSachSanPham.Find(x => x.MaSP == maSP);
+
+                if (sp != null)
+                {
+                    FormSanPham frmSanPham = new FormSanPham(sp);
+                    if (frmSanPham.ShowDialog() == DialogResult.OK)
+                    {
+                        // Cập nhật thông tin sản phẩm
+                        int index = danhSachSanPham.FindIndex(x => x.MaSP == maSP);
+                        if (index != -1)
+                        {
+                            danhSachSanPham[index] = frmSanPham.sanPhamMoi;
+                            HienThiDanhSach();
+                            MessageBox.Show("Đã cập nhật thông tin sản phẩm thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy sản phẩm!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một sản phẩm để sửa!", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SanPham sanPhamMoi = new SanPham();
+            sanPhamMoi.MaSP = txtMaSP.Text;
+            sanPhamMoi.TenSP = txtTenSP.Text;
+            sanPhamMoi.NgayNhap = DateTime.Parse(txtNgayNhap.Text);
+            sanPhamMoi.LoaiSP = txtLoaiSP.Text;
+
+            FormSanPham frmSanPham = new FormSanPham(sanPhamMoi);
+            if (frmSanPham.ShowDialog() == DialogResult.OK)
+            {
+                danhSachSanPham.Add(frmSanPham.sanPhamMoi);
+                HienThiDanhSach();
+            }
+        }
+        private void XoaSanPham(string maSP)
+        {
+            if (danhSachSanPham.RemoveAll(x => x.MaSP == maSP) > 0)
+            {
+                MessageBox.Show("Đã xóa sản phẩm thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                HienThiDanhSach();
+            }
+        }
+        public class SanPham
+        {
+            public string MaSP { get; set; }
+            public string TenSP { get; set; }
+            public DateTime NgayNhap { get; set; }
+            public string LoaiSP { get; set; }
+        }
+    }
+}
---
+++ SQLQuery1 qlsp.sql
@@ -0,0 +1,43 @@
+﻿-- Tạo cơ sở dữ liệu
+CREATE DATABASE De02;
+GO
+
+-- Sử dụng cơ sở dữ liệu vừa tạo
+GO
+
+-- Tạo bảng LoaiSP
+CREATE TABLE LoaiSP (
+    MaLoai char(2) PRIMARY KEY,
+    TenLoai nvarchar(30)
+);
+
+-- Tạo bảng Sanpham
+CREATE TABLE Sanpham (
+    MaSP char(6) PRIMARY KEY,
+    TenSP nvarchar(30),
+    Ngaynhap DateTime,
+    MaLoai char(2),
+    FOREIGN KEY (MaLoai) REFERENCES LoaiSP(MaLoai)
+);
+
+-- Nhập dữ liệu mẫu cho bảng LoaiSP
+INSERT INTO LoaiSP (MaLoai, TenLoai) VALUES
+('DT', N'Điện thoại'),
+('LT', N'Laptop'),
+('TV', N'Tivi'),
+('TL', N'Tủ lạnh');
+
+-- Nhập dữ liệu mẫu cho bảng Sanpham
+INSERT INTO Sanpham (MaSP, TenSP, Ngaynhap, MaLoai) VALUES
+('SP0001', N'iPhone 13', '2023-01-15', 'DT'),
+('SP0002', N'Samsung Galaxy S21', '2023-02-20', 'DT'),
+('SP0003', N'MacBook Pro', '2023-03-10', 'LT'),
+('SP0004', N'Dell XPS 13', '2023-04-05', 'LT'),
+('SP0005', N'Sony Bravia 55"', '2023-05-12', 'TV'),
+('SP0006', N'LG OLED 65"', '2023-06-18', 'TV'),
+('SP0007', N'Panasonic Inverter 300L', '2023-07-22', 'TL'),
+('SP0008', N'Samsung Side-by-Side 620L', '2023-08-30', 'TL');
+
+-- Kiểm tra dữ liệu đã nhập
+SELECT * FROM LoaiSP;
+SELECT * FROM Sanpham;
